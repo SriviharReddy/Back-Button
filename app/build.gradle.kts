4824c8dbba500838cdf5b0a2e182e5b2c4ff35dd
apricot57 <kssr.1990@gmail.com>
2026-05-26 20:44:14 +0530

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.back"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
     }
 
